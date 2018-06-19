a7f79bc04e4caba0b93d876ba8d909f6e863b46a
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-06-19 22:45:23 +1200

    add ability to modify current progress

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 function progressBar(total, percentChar) {
-	this.total = total;
-	this.current = 0;
-	this.percentChar = percentChar;
-	this.barLength = process.stdout.columns - 100;
-	this.render();
+  this.total = total;
+  this.current = 0;
+  this.percentChar = percentChar;
+  this.barLength = process.stdout.columns - 100;
+  this.render();
 }
 
 progressBar.prototype.setTotal = function(total) {
-	this.total = total;
+  this.total = total;
 };
 
-progressBar.prototype.update = function(current) {
-	this.current++;
-	this.render();
+progressBar.prototype.update = function(current = null) {
+  if (current !== null) {
+    this.current += current;
+  } else {
+    this.current++;
+  }
+  this.render();
 };
 
 progressBar.prototype.render = function() {
-	if (this.current > this.total) { throw "current value exceed total" };
-	let percentageProgress = parseFloat((this.current / this.total).toFixed(2));
-	isNaN(percentageProgress) && (percentageProgress = 0);
-	let filledBarLength = Math.floor(percentageProgress * this.barLength);
-	let filledBar = this.percentChar.repeat(filledBarLength);
-	let emptyBar = " ".repeat(this.barLength - filledBarLength);
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-	process.stdout.write(
-		`Current progress: [${filledBar}${emptyBar}] | ${percentageProgress *
-			100}%`
-	);
-	percentageProgress == 1 && console.log();
+  if (this.current > this.total) {
+    throw "current value exceed total";
+  }
+  let percentageProgress = parseFloat((this.current / this.total).toFixed(2));
+  isNaN(percentageProgress) && (percentageProgress = 0);
+  let filledBarLength = Math.floor(percentageProgress * this.barLength);
+  let filledBar = this.percentChar.repeat(filledBarLength);
+  let emptyBar = " ".repeat(this.barLength - filledBarLength);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(
+    `Current progress: [${filledBar}${emptyBar}] | ${percentageProgress * 100}%`
+  );
+  percentageProgress == 1 && console.log();
 };
 
 module.exports = progressBar;
